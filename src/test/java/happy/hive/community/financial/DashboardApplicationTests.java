8244b8a92a02122ed6060c338a168108f721b170
celes <celeste2325@gmail.com>
2024-10-14 21:56:22 -0400

    chore: add configuration files

---
+++ src/test/java/happy/hive/community/financial/DashboardApplicationTests.java
@@ -0,0 +1,15 @@
+package happy.hive.community.financial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    //TODO podrias intentar agrear un par de test
+
+}
